ebb6fc63494f5ee7b0391bc9bc8badf28adb7718
German Mendez <mendezgerman678@gmail.com>
2024-03-12 20:54:04 -0300

    creation

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroesRoutingModule } from './heores-routing.module';
+import { MaterialModule } from '../material/material.module';
+
+
+
+@NgModule({
+  declarations: [
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    MaterialModule
+  ],
+  exports: [
+    LayoutPageComponent
+  ]
+})
+export class HeroesModule { }
